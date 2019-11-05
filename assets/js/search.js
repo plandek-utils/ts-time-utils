632a158003144ffce1a6ce027569c4f553637acb
Eduardo Turiño <eturino@plandek.com>
2019-11-05 11:33:20 +0000

    [ci skip] Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"freezeTime","url":"globals.html#freezetime","classes":"tsd-kind-function tsd-has-type-parameter"},{"id":1,"kind":64,"name":"freezeTimeAwait","url":"globals.html#freezetimeawait","classes":"tsd-kind-function tsd-has-type-parameter"}]};
